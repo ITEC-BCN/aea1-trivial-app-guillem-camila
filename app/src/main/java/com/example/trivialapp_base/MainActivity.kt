f0a29b9cd0f6977e7fe144b6c3610f0f5558e31f
GuillemSoto <guillem.soto.7e8@itb.cat>
2026-01-15 09:34:19 +0100

    feat (navigation, imports): El NavController i Routes funcionen

--- app/src/main/java/com/example/trivialapp_base/MainActivity.kt
+++ app/src/main/java/com/example/trivialapp_base/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +12,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.trivialapp_base.ui.theme.TrivialAPP_BaseTheme
+import com.example.trivialapp_base.view.GameScreen
+import com.example.trivialapp_base.view.MenuScreen
+import com.example.trivialapp_base.view.ResultScreen
+import com.example.trivialapp_base.view.SplashScreen
+import com.example.trivialapp_base.viewmodel.GameViewModel
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +30,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrivialAPP_BaseTheme {
                 // Controlador de navegación
-
+                val myViewModel by viewModels < GameViewModel>()
+                val navigationController = rememberNavController()
+                NavHost(
+                    navController = navigationController,
+                    startDestination = Routes.MenuScreen.route
+                ) {
+                    composable(Routes.MenuScreen.route) { MenuScreen(navigationController, myViewModel) }
+                    composable(Routes.GameScreen.route) { GameScreen(navigationController, myViewModel) }
+                    composable(Routes.ResultScreen.route) { ResultScreen(navigationController, myViewModel) }
+                    composable(Routes.SplashScreen.route) { SplashScreen(navigationController) }
+                }
 
 
                 // Instanciamos el ViewModel una vez
--- app/src/main/java/com/example/trivialapp_base/Routes.kt
+++ app/src/main/java/com/example/trivialapp_base/Routes.kt
@@ -1,5 +1,8 @@
 package com.example.trivialapp_base
 
 sealed class Routes(val route: String) {
-
+    object MenuScreen:Routes("Screen_menu")
+    object GameScreen:Routes("Screen_game")
+    object ResultScreen:Routes("Screen_result")
+    object SplashScreen:Routes("Screen_splash")
 }
