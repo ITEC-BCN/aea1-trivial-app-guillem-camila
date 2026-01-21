b2c65b8ec8879308266fda3b3ffedf0066406d34
GuillemSoto <guillem.soto.7e8@itb.cat>
2026-01-21 09:05:46 +0100

    feat (GameScree): Game screen v1

--- app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
@@ -9,35 +9,56 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import com.example.trivialapp_base.Routes
+import com.example.trivialapp_base.model.ProveedorPreguntas
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun GameScreen(navController: NavController, viewModel: GameViewModel) {
+    val currentQuestion = ProveedorPreguntas.obtenerPreguntas().random()
+
     Box(
         modifier = Modifier.fillMaxSize().background(Color.Cyan)) {
         Column(
             modifier = Modifier.fillMaxSize()
         ) {
             Text(
-                text = "Pregunta 1"
+                text = currentQuestion.pregunta
             )
+
             Box(
                 modifier = Modifier.fillMaxSize()
             ){
                 Column() {
                     Row() {
-                        Button(onClick = { viewModel.puntuacion++ } ) { }
-                        Button(onClick = { viewModel.puntuacion++ } ) { }
+                        Button(onClick = { if (currentQuestion.respuesta1==currentQuestion.respuestaCorrecta) {
+                            viewModel.puntuacion++
+                        }
+
+                        } ) { Text(currentQuestion.respuesta1) }
+                        Button(onClick = { if (currentQuestion.respuesta2==currentQuestion.respuestaCorrecta)
+                        {
+                            viewModel.puntuacion++
+                        } } ) { Text(currentQuestion.respuesta2) }
                     }
                     Row() {
-                        Button(onClick = {viewModel.puntuacion++ } ) { }
-                        Button(onClick = {viewModel.puntuacion++ } ) { }
+                        Button(onClick = { if (currentQuestion.respuesta3==currentQuestion.respuestaCorrecta)
+                        {
+                            viewModel.puntuacion++
+                        }} ) { Text(currentQuestion.respuesta3) }
+                        Button(onClick = { if (currentQuestion.respuesta4==currentQuestion.respuestaCorrecta)
+                        {
+                            viewModel.puntuacion++
+                        } } ) { Text(currentQuestion.respuesta4) }
                     }
+                    Text(viewModel.puntuacion.toString())
                 }
             }
         }
